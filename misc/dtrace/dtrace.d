a7507516a3c0b0adc9dab023a28a698a841fdd86
Kazuho Oku <kazuhooku@gmail.com>
2019-05-05 10:03:35 +0900

    hexdump the handshake message body

--- misc/dtrace/dtrace.d
+++ misc/dtrace/dtrace.d
@@ -10,3 +10,7 @@ picotls$target:::client_random {
 picotls$target:::new_secret {
     printf("\n{\"addr\": \"0x%p\", \"event\": \"new_secret\", \"label\": \"%s\", \"secret\": \"%s\"}", arg0, copyinstr(arg1), copyinstr(arg2));
 }
+picotls$target:::receive_message {
+    printf("\n{\"addr\": \"0x%p\", \"event\": \"receive_message\", \"type\": %d, \"ret\": %d}\n", arg0, arg1, arg4);
+    tracemem(copyin(arg2, arg3), 65535, arg3);
+}
