3327644fdd493d959b81edc3ada591b35aba4ade
Lars Eggert <lars@eggert.org>
2019-07-03 12:19:16 +0300

    Add EXTERNAL_READ_ENTROPY define.
    This lets one provide a substitute for the default read_entopy() call, e.g.,  on
    embedded platforms where /dev/urandom and /dev/random are unavailable.

--- lib/cifra.c
+++ lib/cifra.c
@@ -89,6 +89,8 @@ static void read_entropy(uint8_t *entropy, size_t size)
     }
 }
 #endif
+#elif defined(EXTERNAL_READ_ENTROPY)
+/* read_entropy() provided elsewhere */
 #else
 static void read_entropy(uint8_t *entropy, size_t size)
 {
