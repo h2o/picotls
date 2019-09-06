ad918d237820717726ea8d8eba878a268821c2bd
Kazuho Oku <kazuhooku@gmail.com>
2019-09-06 14:38:58 +0900

    don't use the callbacks, as it's hard to setup the back pointers and the structures at an early moment

--- lib/picotls.c
+++ lib/picotls.c
@@ -93,10 +93,7 @@
 #endif
 
 #if PICOTLS_USE_DTRACE
-#define PTLS_SHOULD_PROBE(LABEL, tls)                                                                                              \
-    (PTLS_UNLIKELY(PICOTLS_##LABEL##_ENABLED()) &&                                                                                 \
-     (tls->ctx->is_traced == NULL || tls->ctx->is_traced->cb(tls->ctx->is_traced, tls)))
-
+#define PTLS_SHOULD_PROBE(LABEL, tls) (PTLS_UNLIKELY(PICOTLS_##LABEL##_ENABLED()) && (tls)->is_traced)
 #define PTLS_PROBE0(LABEL, tls)                                                                                                    \
     do {                                                                                                                           \
         ptls_t *_tls = (tls);                                                                                                      \
@@ -233,6 +230,7 @@ struct st_ptls_t {
     unsigned send_change_cipher_spec : 1;
     unsigned needs_key_update : 1;
     unsigned key_update_send_request : 1;
+    unsigned is_traced : 1;
     /**
      * misc.
      */
@@ -4272,6 +4270,16 @@ int ptls_is_psk_handshake(ptls_t *tls)
 void **ptls_get_data_ptr(ptls_t *tls)
 {
     return &tls->data_ptr;
+}
+
+int ptls_is_traced(ptls_t *tls)
+{
+    return tls->is_traced;
+}
+
+void ptls_set_is_traced(ptls_t *tls, int is_traced)
+{
+    tls->is_traced = is_traced;
 }
 
 static int handle_handshake_message(ptls_t *tls, ptls_message_emitter_t *emitter, ptls_iovec_t message, int is_end_of_record,
