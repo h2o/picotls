0b87c20d5bbe164ba851f497d30fd774b4b40881
Kazuho Oku <kazuhooku@gmail.com>
2021-04-17 17:13:08 +0900

    remove unnecessary `if`

--- lib/openssl.c
+++ lib/openssl.c
@@ -1237,13 +1237,11 @@ static int verify_cert_chain(X509_STORE *store, X509 *cert, STACK_OF(X509) * cha
         /* when _acting_ as client, set the server name */
         if (!is_server) {
             assert(server_name != NULL && "ptls_set_server_name MUST be called");
-            if (server_name != NULL) {
-                if (ptls_server_name_is_ipaddr(server_name)) {
-                    X509_VERIFY_PARAM_set1_ip_asc(params, server_name);
-                } else {
-                    X509_VERIFY_PARAM_set1_host(params, server_name, 0);
-                    X509_VERIFY_PARAM_set_hostflags(params, X509_CHECK_FLAG_NO_PARTIAL_WILDCARDS);
-                }
+            if (ptls_server_name_is_ipaddr(server_name)) {
+                X509_VERIFY_PARAM_set1_ip_asc(params, server_name);
+            } else {
+                X509_VERIFY_PARAM_set1_host(params, server_name, 0);
+                X509_VERIFY_PARAM_set_hostflags(params, X509_CHECK_FLAG_NO_PARTIAL_WILDCARDS);
             }
         }
     }
