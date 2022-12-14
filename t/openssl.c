b80fc4439ebb671af0d491d4a1239b0c19a59e11
Kazuho Oku <kazuhooku@gmail.com>
2022-12-14 15:32:28 +0900

    update test

--- t/openssl.c
+++ t/openssl.c
@@ -464,10 +464,12 @@ static void many_handshakes(void)
                 if (num_issued < num_total)
                     qat_set_pending(offending);
                 break;
-            case PTLS_ERROR_ASYNC_OPERATION:
-                qat.conns[offending].wait_fd = ptls_openssl_get_async_fd(qat.conns[offending].tls);
+            case PTLS_ERROR_ASYNC_OPERATION: {
+                ptls_async_job_t *job = ptls_get_async_job(qat.conns[offending].tls);
+                assert(job->get_fd != NULL);
+                qat.conns[offending].wait_fd = job->get_fd(job);
                 assert(qat.conns[offending].wait_fd != -1);
-                break;
+            } break;
             default:
                 fprintf(stderr, "ptls_handshake returned %d\n", hsret);
                 abort();
