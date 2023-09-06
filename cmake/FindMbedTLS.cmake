4a01610e8233d06b3f16ae3a781b3627b4b5d56d
Christian Huitema <huitema@huitema.net>
2023-09-06 11:14:21 -0700

    Update MbedTLS find

--- cmake/FindMbedTLS.cmake
+++ cmake/FindMbedTLS.cmake
@@ -8,14 +8,24 @@ find_path(MBEDTLS_INCLUDE_DIRS
         ${CMAKE_BINARY_DIR}/../mbedtls/include/
         ../mbedtls/include/ )
 
-set(MBEDTLS_HINTS ${MBEDTLS_PREFIX}/build ${CMAKE_BINARY_DIR}/../mbedtls/build ../mbedtls/build)
+
+
+set(MBEDTLS_HINTS ${MBEDTLS_PREFIX}/build/library 
+    ${CMAKE_BINARY_DIR}/../mbedtls/build/library
+    ../mbedtls/build/library ../mbedtls/library)
 
 find_library(MBEDTLS_LIBRARY mbedtls HINTS ${MBEDTLS_HINTS})
+find_library(MBEDTLS_CRYPTO mbedcrypto HINTS ${MBEDTLS_HINTS})
+find_library(MBEDTLS_X509 mbedx509 HINTS ${MBEDTLS_HINTS})
 include(FindPackageHandleStandardArgs)
 # handle the QUIETLY and REQUIRED arguments and set PTLS_FOUND to TRUE
 # if all listed variables are TRUE
 find_package_handle_standard_args(MbedTLS REQUIRED_VARS
     MBEDTLS_LIBRARY
+    MBEDTLS_CRYPTO
+    MBEDTLS_X509
     MBEDTLS_INCLUDE_DIRS)
-
-mark_as_advanced(MBEDTLS_LIBRARY MBEDTLS_INCLUDE_DIRS)
+    
+if(MbedTLS_FOUND)
+    set(MBEDTLS_LIBRARIES ${MBEDTLS_LIBRARY} ${MBEDTLS_CRYPTO} ${MBEDTLS_X509})
+    mark_as_advanced(MBEDTLS_LIBRARIES MBEDTLS_INCLUDE_DIRS)
