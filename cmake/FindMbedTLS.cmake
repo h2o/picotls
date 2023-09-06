be89606ccfe7923e9903786f405abd21ba43775d
Christian Huitema <huitema@huitema.net>
2023-09-06 11:04:36 -0700

    Add find package

--- cmake/FindMbedTLS.cmake
+++ cmake/FindMbedTLS.cmake
@@ -11,5 +11,11 @@ find_path(MBEDTLS_INCLUDE_DIRS
 set(MBEDTLS_HINTS ${MBEDTLS_PREFIX}/build ${CMAKE_BINARY_DIR}/../mbedtls/build ../mbedtls/build)
 
 find_library(MBEDTLS_LIBRARY mbedtls HINTS ${MBEDTLS_HINTS})
+include(FindPackageHandleStandardArgs)
+# handle the QUIETLY and REQUIRED arguments and set PTLS_FOUND to TRUE
+# if all listed variables are TRUE
+find_package_handle_standard_args(MbedTLS REQUIRED_VARS
+    MBEDTLS_LIBRARY
+    MBEDTLS_INCLUDE_DIRS)
 
 mark_as_advanced(MBEDTLS_LIBRARY MBEDTLS_INCLUDE_DIRS)
